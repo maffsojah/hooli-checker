f37f0c26cd26dec0ec44052a3338e8cc62f57d46
maffsojah <maffsojah1@gmail.com>
2019-05-13 10:49:49 +0200

    generate the set-difference

--- index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs'); // useful for navigating the file system
-const parse = require('csv-parse/lib/sync'); // needed for parsing CSV file data
-
-function linkBuyerToFacility() {
-  // your solution goes here
-}
-
-linkBuyerToFacility();
